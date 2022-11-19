f6e7d1637c1cab5733d9023bf7f10ee1f5ee52aa
Oren Bigler <bigoreo@gmail.com>
2022-11-19 17:55:55 +0200

    feat: adding LED control, feat: MFRC functions in h file

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <MFRC522.h>
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
+#include <FastLED.h>
 
 // logic
 #define MY_LEVEL 1
@@ -9,7 +10,9 @@
 // RFID
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
+#include "MFRC522_func.h"
+
 MFRC522::MIFARE_Key key; 
 // Init array that will store new NUID 
 byte nuidPICC[4];
@@ -17,58 +20,21 @@ byte blockAddr      = 4;
 byte buffer[18];
 byte size = sizeof(buffer);
 byte trailerBlock   = 7;
+bool read_success, write_success, auth_success;
 
 // sound
 SoftwareSerial mySoftwareSerial(2, 3); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
+#define VOLUME 15
 
-bool write_and_verify(byte blockAddr, byte dataBlock[], byte buffer[], byte size) {
-    MFRC522::StatusCode status;
+// LEDs
+#define DATA_PIN 7
+#define NUM_LEDS 3
+#define BRIGHTNESS 50
 
-    // Write data to the block
-    Serial.print(F("Writing data into block ")); Serial.print(blockAddr);
-    Serial.println(F(" ..."));
-    // dump_byte_array(dataBlock, 16); Serial.println();
-    status = (MFRC522::StatusCode) rfid.MIFARE_Write(blockAddr, dataBlock, 16);
-    if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("MIFARE_Write() failed: "));
-        Serial.println(rfid.GetStatusCodeName(status));
-        return false;
-    }
-    Serial.println();
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
-    // Read data from the block (again, should now be what we have written)
-    Serial.print(F("Reading data from block ")); Serial.print(blockAddr);
-    Serial.println(F(" ..."));
-    status = (MFRC522::StatusCode) rfid.MIFARE_Read(blockAddr, buffer, &size);
-    if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("MIFARE_Read() failed: "));
-        Serial.println(rfid.GetStatusCodeName(status));
-        return false;
-    }
-    // Serial.print(F("Data in block ")); Serial.print(blockAddr); Serial.println(F(":"));
-    // dump_byte_array(buffer, 16); Serial.println();
-
-    // Check that data in block is what we have written
-    // by counting the number of bytes that are equal
-    Serial.println(F("Checking result..."));
-    byte count = 0;
-    for (byte i = 0; i < 16; i++) {
-        // Compare buffer (= what we've read) with dataBlock (= what we've written)
-        if (buffer[i] == dataBlock[i])
-            count++;
-    }
-    Serial.print(F("Number of bytes that match = ")); Serial.println(count);
-    if (count == 16) {
-        Serial.println(F("Success :-)"));
-        return true;
-    } else {
-        Serial.println(F("Failure, no match :-("));
-        Serial.println(F("  perhaps the write didn't work properly..."));
-        Serial.println();
-        return false;
-    }
-}
 
 void setup() { 
 
@@ -76,10 +42,12 @@ void setup() {
 
   // RFID
   SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
+  mfrc522.PCD_Init(); // Init MFRC522
+  //mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
+  Serial.print("Station level is: "); Serial.println(MY_LEVEL);
 
   // Sound
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
@@ -87,51 +55,61 @@ void setup() {
   
   mySoftwareSerial.begin(9600);
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
-    Serial.println(F("Unable to begin:"));
+    Serial.println(F("Unable to begin DFPlayer Mini:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
-    while(true){
-      delay(0); // Code to compatible with ESP8266 watch dog.
-    }
+    Serial.println(F("Continuing with no sound..."));
+  } else {
+    Serial.println(F("DFPlayer Mini online."));
+    myDFPlayer.volume(VOLUME);  //Set volume value. From 0 to 30
   }
-  Serial.println(F("DFPlayer Mini online."));
-  
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+
+  // LEDs
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
 }
  
 void loop() {
 
+  // clearing LEDs if the sound is not playing
+  if (myDFPlayer.readState() != Busy) {
+    FastLED.clear();
+    FastLED.show();
+  };
+
   // Look for new cards
-  if ( ! rfid.PICC_IsNewCardPresent())
+  if ( ! mfrc522.PICC_IsNewCardPresent())
     return;
 
   // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
+  if ( ! mfrc522.PICC_ReadCardSerial())
     return;
 
-  // authenticate!
-  MFRC522::StatusCode auth_status = (MFRC522::StatusCode) rfid.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(rfid.uid));
-  if (auth_status != MFRC522::STATUS_OK) {
-    Serial.print(F("PCD_Authenticate() failed: "));
-    Serial.println(rfid.GetStatusCodeName(auth_status));
+  // perform authentication to open communication
+  auth_success = authenticate(trailerBlock, key);
+  if (!auth_success) {
     return;
   }
 
-  // MIFARE_Read get the size and changes it.
+  // RFID reads get the size and change it.
   // we have to restore it before every call
   size = sizeof(buffer);
-  MFRC522::StatusCode read_status = (MFRC522::StatusCode) rfid.MIFARE_Read(blockAddr, buffer, &size);
-  if (read_status != MFRC522::STATUS_OK) {
-      Serial.print(F("MIFARE_Read() failed: "));
-      Serial.println(rfid.GetStatusCodeName(read_status));
-      return;
+  // read the tag to get coded information to buffer
+  read_success = read_block(blockAddr, buffer, size);
+  if (!read_success) {
+    // Serial.println(F("Initial read failed, closing connection"));
+    // Halt PICC
+    mfrc522.PICC_HaltA();
+    // Stop encryption on PCD
+    mfrc522.PCD_StopCrypto1();
+    return;
   }
 
   byte level = *(buffer + 1);
   if(level == 0xff) {
     level = 0;
   }
-  Serial.print("Current level: "); Serial.println(level);
+  Serial.print("Current chip level: "); Serial.println(level);
 
   if(level == MY_LEVEL - 1) {
     // happy path
@@ -146,7 +124,7 @@ void loop() {
 
     bool write_success = write_and_verify(blockAddr, dataBlock, buffer, size);
     if(!write_success) {
-      // play - cound not write. try again
+      // play - could not write. try again
       myDFPlayer.play(4);
       Serial.println(F("write failed"));
       return;
@@ -169,9 +147,16 @@ void loop() {
   }
 
   // Halt PICC
-  rfid.PICC_HaltA();
+  mfrc522.PICC_HaltA();
 
   // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+  mfrc522.PCD_StopCrypto1();
+
+  // visual indication for a successful operation
+  fill_solid(leds, NUM_LEDS, CHSV(0, 255, 255));
+  FastLED.show();
+      
+  // hold everything in place for some time
+  delay(200);
 }
 
